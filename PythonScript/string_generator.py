a15626e7771ac9e0129246b8fcf90d8028f4c2e2
r.rosica <prova@prova.it>
2019-01-06 18:18:14 +0100

    -Ottimizzata galleria con anteprime -Aggiornate info vattela

--- PythonScript/string_generator.py
+++ PythonScript/string_generator.py
@@ -2,4 +2,4 @@ num = 44
 
 for x in range(1, num):
     print("<li><a href=\"assets/img/gallery/g (%s).jpg\" class=\"fancybox\" data-fancybox-group=\"gallery\"><img src=\"assets/"
-          "img/gallery/g (%s).jpg\" alt=\"\" /></a></li>" % (x, x))
+          "img/gallery/thumbs/g (%s).jpg\" alt=\"\" /></a></li>" % (x, x))
